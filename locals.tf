ffabdbd4d72cfa24d9dc8ac12b5ccb95df0f82bc
Gus Narea <gnarea@users.noreply.github.com>
2024-09-10 20:09:54 +0100

    fix: Remove main branch protection
    As we haven't yet upgraded to Pro/Enterprise.

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  // Work around https://github.com/integrations/terraform-provider-github/issues/1009
-  github_actions_app_node_id = "MDM6QXBwMTUzNjg=" // https://api.github.com/apps/github-actions
-}
--- variables.tf
+++ variables.tf
@@ -8,13 +8,3 @@ variable "homepage_url" {
   default     = ""
   description = "GitHub project homepage"
 }
-variable "ci_contexts" {
-  default     = []
-  type        = list(string)
-  description = "Required CI contexts for PRs to merged in the main branch"
-}
-
-variable "support_releases" {
-  default     = true
-  description = "Whether the project uses Semantic Releases"
-}
